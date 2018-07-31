2dad6825bf6eccacc24c28707affe456d1c27fe1
Vadim Lizogub <vadlvadl789@gmail.com>
2018-07-31 12:46:42 +0300

    [QAAUT6-1] - IssuePage rewritten to Selenide
    Source:
    https://github.com/medvedievigor/Jira-Selenide-Test-/blob/master/src/test/java/pages/IssuePage.java

--- src/test/java/JiraTestNewIssue.java
+++ src/test/java/JiraTestNewIssue.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -9,50 +10,43 @@ import pages.LoginPage;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class JiraTestNewIssue {
 
     protected WebDriver driver;
 
     @Test
     public void createNewTaskTest(){
-        try{
-            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                    DesiredCapabilities.chrome());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.browser = "chrome";
+        Configuration.timeout = 7000;
 
         // Run browser and get needed address
-        driver.get("http://jira.hillel.it:8080/");
+        open("http://jira.hillel.it:8080/");
 
-        LoginPage loginPage = new LoginPage(driver);
+        LoginPage loginPage = new LoginPage();
 
         loginPage.enterLogin("webinar5");
         loginPage.enterPassword("webinar5");
         loginPage.clickSubmit();
 
-
         DashboardPage dashboardPage = new DashboardPage();
-        // Add methods to wait until page is loaded
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         dashboardPage.clickCreateIssueButton();
 
-        IssuePage issuePage = new IssuePage(driver);
+//        IssuePage issuePage = new IssuePage(driver);
 
 
-        try {
-            Thread.sleep(2500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        issuePage.enterSummary("[Test Automation] QAAUTO6-T1_test01");
-        issuePage.enterDescription("Testing issue created according to http://jira.hillel.it:8080/browse/QAAUT6-1 task");
-        issuePage.clickAssignToMe();
-        issuePage.clickCreateButton();
+//        try {
+//            Thread.sleep(2500);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        issuePage.enterSummary("[Test Automation] QAAUTO6-T1_test01");
+//        issuePage.enterDescription("Testing issue created according to http://jira.hillel.it:8080/browse/QAAUT6-1 task");
+//        issuePage.clickAssignToMe();
+//        issuePage.clickCreateButton();
 
         try {
             Thread.sleep(5000);
@@ -60,7 +54,5 @@ public class JiraTestNewIssue {
             e.printStackTrace();
         }
 
-
-        driver.close();
     }
 }
