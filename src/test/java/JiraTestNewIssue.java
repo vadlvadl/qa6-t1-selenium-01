f0c908d399a79198063354aa3c49493db00aa1bc
Vadim Lizogub <vadlvadl789@gmail.com>
2018-07-31 12:49:17 +0300

    [QAAUT6-1] - TestNewIssue has been rewritten to Selenide

--- src/test/java/JiraTestNewIssue.java
+++ src/test/java/JiraTestNewIssue.java
@@ -1,20 +1,13 @@
 import com.codeborne.selenide.Configuration;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+
 import org.testng.annotations.Test;
 import pages.DashboardPage;
 import pages.IssuePage;
 import pages.LoginPage;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import static com.codeborne.selenide.Selenide.open;
 
 public class JiraTestNewIssue {
-
-    protected WebDriver driver;
 
     @Test
     public void createNewTaskTest(){
@@ -35,18 +28,12 @@ public class JiraTestNewIssue {
         DashboardPage dashboardPage = new DashboardPage();
         dashboardPage.clickCreateIssueButton();
 
-//        IssuePage issuePage = new IssuePage(driver);
+        IssuePage issuePage = new IssuePage();
 
-
-//        try {
-//            Thread.sleep(2500);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        issuePage.enterSummary("[Test Automation] QAAUTO6-T1_test01");
-//        issuePage.enterDescription("Testing issue created according to http://jira.hillel.it:8080/browse/QAAUT6-1 task");
-//        issuePage.clickAssignToMe();
-//        issuePage.clickCreateButton();
+        issuePage.enterSummary("[Test Automation] QAAUTO6-T1_test02");
+        issuePage.enterDescription("Testing issue created according to http://jira.hillel.it:8080/browse/QAAUT6-1 task");
+        issuePage.clickAssignToMe();
+        issuePage.clickCreateButton();
 
         try {
             Thread.sleep(5000);
