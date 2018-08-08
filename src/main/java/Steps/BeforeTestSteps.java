5a52aa84d3f2c30087c6e1e256d66e223d4eec81
Igor <37653488+medvedievigor@users.noreply.github.com>
2018-08-08 14:15:46 +0300

    Added for newIssueTest and findIssueTest
    Added class was created for using its metod in every test class before test

---
+++ src/main/java/Steps/BeforeTestSteps.java
@@ -0,0 +1,25 @@
+package Steps;
+
+import com.codeborne.selenide.Configuration;
+import pages.LoginPage;
+import helpers.LoadProperties;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class BeforeTestSteps {
+
+    public void setupAndLogin () {
+
+        LoginPage loginPage = new LoginPage();
+        LoadProperties loadProperties = new LoadProperties();
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.browser = "chrome";
+        Configuration.timeout = 9000;
+
+        open(loadProperties.getProjectProperty());
+
+        loginPage.enterUserName(loadProperties.getUserName());
+        loginPage.enterPassword(loadProperties.getPassword());
+        loginPage.clickSubmitButton();
+    }
+}
