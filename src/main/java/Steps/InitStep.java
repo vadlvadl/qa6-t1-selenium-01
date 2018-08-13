d089003e2c071cfb7e1ebc281f75dfeb1dbc65ec
Vadim Lizogub <vadlvadl789@gmail.com>
2018-08-13 10:25:31 +0300

    InitStep added

---
+++ src/main/java/Steps/InitStep.java
@@ -0,0 +1,23 @@
+package Steps;
+
+import com.codeborne.selenide.Configuration;
+import helpers.PropertyReader;
+
+public class InitStep {
+
+    private PropertyReader appConfig;
+    private PropertyReader credentials;
+
+    public void initConfiguration(){
+
+        // Get full path to root directory
+        String rootPath = System.getProperty("user.dir");
+
+        appConfig = new PropertyReader(rootPath + "/jira.properties");
+        credentials = new PropertyReader(rootPath + "/credentials.properties");
+
+        Configuration.remote = appConfig.getStringValue("configurationRemote");
+        Configuration.browser = appConfig.getStringValue("configurationBrowser");
+        Configuration.timeout = appConfig.getIntValue("configurationTimeout");
+    }
+}
